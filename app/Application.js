25e81ac1408459ede287e3d60cf6de78ccbfa12f
Marcel van den Bosch <m.v.d.bosch@gmail.com>
2016-07-31 23:25:42 +0200

    first version of yql implementation

--- app/Application.js
+++ app/Application.js
@@ -13,8 +13,8 @@ Ext.define('Ads.Application', {
     ],
     
     launch: function () {
-        // TODO - Launch the application
-    },
+		
+	},
 
     onAppUpdate: function () {
         Ext.Msg.confirm('Application Update', 'This application has an update, reload?',
--- app/view/main/MainController.js
+++ app/view/main/MainController.js
@@ -9,6 +9,47 @@ Ext.define('Ads.view.main.MainController', {
 
     alias: 'controller.main',
 
+	config: {
+		data: null
+	},
+
+	init: function () {
+		// select * from html where url='http://www.te-les-koop.nl/advertenties.php' and xpath='/html/body/table/tbody/tr/td[1]/table/tbody/tr/td/table/tbody'
+		var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwww.te-les-koop.nl%2Fadvertenties.php%22%20and%20xpath%3D'%2Fhtml%2Fbody%2Ftable%2Ftbody%2Ftr%2Ftd%5B1%5D%2Ftable%2Ftbody%2Ftr%2Ftd%2Ftable%2Ftbody'&format=json&diagnostics=true&callback=",
+			me = this,
+			deferred = new Ext.Deferred(),
+			xhr = new XMLHttpRequest();
+
+		xhr.open('get', url, true);
+		xhr.responseType = 'json';
+		xhr.onload = function() {
+			var status = xhr.status;
+			if (status === 200) {
+				deferred.resolve(xhr.response);
+			} else {
+				deferred.reject(status);
+			}
+		};
+		xhr.send();
+
+		Ext.Deferred.all([deferred.promise]).then(function(response) {
+			this.processData(response);
+		}, null, null, me);
+	},
+
+	processData: function (data) {
+		data = data[0];
+		this.setData(data.query.results.tbody.tr);
+		this.processRows();
+	},
+
+	processRows: function() {
+		var rows = this.getData();
+		for (var i = 0; i <= rows.length; i++) {
+			debugger;
+		}
+	},
+
     onItemSelected: function (sender, record) {
         Ext.Msg.confirm('Confirm', 'Are you sure?', 'onConfirm', this);
     },
