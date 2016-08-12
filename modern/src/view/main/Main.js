ee47cad65d164f133dc23ec576ab9086e6910c7c
Marcel van den Bosch <m.v.d.bosch@gmail.com>
2016-08-12 10:36:11 +0200

    Tabbar now better visible and added a titlebar which displays where you are

--- modern/src/view/main/Main.js
+++ modern/src/view/main/Main.js
@@ -28,59 +28,102 @@ Ext.define('Ads.view.main.Main', {
 
     tabBarPosition: 'bottom',
 
-    items: [
-        {
-            title: 'Aangeboden',
+    items: [ 
+		{
             iconCls: 'x-fa fa-tag',
             layout: 'fit',
-            items: [{
-                xtype: 'list',
-				store: 'Aangeboden',
-				itemTpl: '<div class=\"advertentie\">' +
-						 '<div class=\"left\">' +
-						 '{titel} <br>' +
-						 '{aanbieder} - {prijs} <br>' +
-						 '{geplaatst}' +
-						 '</div>' +
-						 '</div>'
-            }]
-        },
-        {
-          title: 'Gevraagd',
-          iconCls: 'x-fa fa-bullseye',
-          layout: 'fit',
-          items: [{
-            xtype: 'list',
-			store: 'Gevraagd',
-			itemTpl: '{titel}'
-          }]
-        },{
-            title: 'Ruilen',
+            items: [ {
+				xtype: 'panel',
+				title: '<div style="text-align:center;">Aangeboden</div>',
+				layout: 'fit',
+				items: [ {
+					xtype: 'list',
+					store: 'Aangeboden',
+					itemTpl: '<div class=\"advertentie\">' +
+							 '<div class=\"left\">' +
+							 '{titel} <br>' +
+							 '{aanbieder} - {prijs} <br>' +
+							 '{geplaatst}' +
+							 '</div>' +
+							 '</div>'
+				} ]
+			} ]
+        }, {
+			iconCls: 'x-fa fa-bullseye',
+			layout: 'fit',
+			items: [ {
+				xtype: 'panel',
+				title: '<div style="text-align:center;">Gevraagd</div>',
+				layout: 'fit',
+				items: [ {
+					xtype: 'list',
+					store: 'Gevraagd',
+					itemTpl: '<div class=\"advertentie\">' +
+							 '<div class=\"left\">' +
+							 '{titel} <br>' +
+							 '{aanbieder} - {prijs} <br>' +
+							 '{geplaatst}' +
+							 '</div>' +
+							 '</div>'
+				} ]
+			} ]
+        }, {
             iconCls: 'x-fa fa-balance-scale',
 			layout: 'fit',
-			items: [{
-				xtype: 'list',
-				store: 'Ruilen',
-				itemTpl: '{titel}'
-			}]
-        },{
-            title: 'Oproepn',
+			items: [ {
+				xtype: 'panel',
+				title: '<div style="text-align:center;">Ruilen</div>',
+				layout: 'fit',
+				items: [ {
+					xtype: 'list',
+					store: 'Ruilen',
+					itemTpl: '<div class=\"advertentie\">' +
+							 '<div class=\"left\">' +
+							 '{titel} <br>' +
+							 '{aanbieder} - {prijs} <br>' +
+							 '{geplaatst}' +
+							 '</div>' +
+							 '</div>'
+				} ]
+			} ]
+        }, {
             iconCls: 'x-fa fa-bullhorn',
 			layout: 'fit',
-			items: [{
-				xtype: 'list',
-				store: 'Oproepen',
-				itemTpl: '{titel}'
-			}]
-        },{
-            title: 'Overig',
+			items: [ {
+				xtype: 'panel',
+				title: '<div style="text-align:center;">Oproepen</div>',
+				layout: 'fit',
+				items: [ {
+					xtype: 'list',
+					store: 'Oproepen',
+					itemTpl: '<div class=\"advertentie\">' +
+							 '<div class=\"left\">' +
+							 '{titel} <br>' +
+							 '{aanbieder} - {prijs} <br>' +
+							 '{geplaatst}' +
+							 '</div>' +
+							 '</div>'
+				} ]
+			} ]
+        }, {
             iconCls: 'x-fa fa-question',
 			layout: 'fit',
-			items: [{
-				xtype: 'list',
-				store: 'Overig',
-				itemTpl: '{titel}'
-			}]
+			items: [ {
+				xtype: 'panel',
+				title: '<div style="text-align:center;">Overig</div>',
+				layout: 'fit',
+				items: [ {
+					xtype: 'list',
+					store: 'Overig',
+					itemTpl: '<div class=\"advertentie\">' +
+							 '<div class=\"left\">' +
+							 '{titel} <br>' +
+							 '{aanbieder} - {prijs} <br>' +
+							 '{geplaatst}' +
+							 '</div>' +
+							 '</div>'
+				} ]
+			} ]
         }
     ]
 });
